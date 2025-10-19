9d0eb88e412ea6cfab58363c0502b276a444b5b0
zhazhengqiang <292865570@qq.com>
2025-10-19 16:43:07 +0800

    initial commit

---
+++ src/main/java/com/qaing/springai/springaidemo/SpringaiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.qaing.springai.springaidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiDemoApplication.class, args);
+    }
+}
